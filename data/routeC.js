29165c3cae9b4e6a07b6c6c3035a325058161a5b
jahidhasansabbir <jahidhasansabbir1@gmail.com>
2026-02-25 18:41:49 +0600

    fixes some error

---
+++ data/routeC.js
@@ -0,0 +1,18 @@
+module.exports = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        route: "C"
+      },
+      geometry: {
+        type: "LineString",
+        coordinates: [
+          [90.40206, 24.75941],
+          [90.40244, 24.75919]
+        ]
+      }
+    }
+  ]
+};
